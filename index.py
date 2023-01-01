772a37b131ceeb93f163792b52d47944516082d9
FelpBSouza <fsouzab13@hotmail.com>
2023-01-01 16:47:28 -0300

    Math Operation

--- index.py
+++ index.py
@@ -1,1 +1,15 @@
 print ('primeiro hello world em python')
+
+a = 10
+b = 5
+
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b 
+divisao = a / b
+resto = a % b
+print (soma)
+print (subtracao)
+print (multiplicacao)
+print (divisao)
+print (resto)
