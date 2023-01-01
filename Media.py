a170a5fa5f94a71c5373bfdd7c734afb70f883e0
FelpBSouza <fsouzab13@hotmail.com>
2023-01-01 17:35:10 -0300

    Average in Python

---
+++ Media.py
@@ -0,0 +1,20 @@
+a = float(input('Primeira nota:'))
+if a >10:
+    a = float(input('Você digitou errado a sua primeira nota.Digite novamente a primeira nota:'))
+b = float(input('Segunda nota:'))
+if b > 10:
+    b = float(input('Você digitou errado a sua segunda nota.Digite novamente a segunda nota:'))
+c = float(input('Terceira nota:'))
+if c > 10:
+    c = float(input('Você digitou errado a sua terceira nota.Digite novamente a terceira nota:'))
+d = float(input('Quarta nota:'))
+if d > 10:
+    d = float(input('Você digitou errado a sua quarta nota.Digite novamente a quarta nota:'))
+
+media = (a + b +c +d)/4
+print ('media: {}'.format(media))
+
+""" if a <= 10 and b <= 10 and c <= 10 and d <= 10:
+    print ('media: {}' .format(media))
+else:
+    print ('Foi informado alguma nota errada:') """
