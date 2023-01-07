73a4fb0cc7bbac0d682a9b6eb7cfb3a3a8c027be
FelpBSouza <fsouzab13@hotmail.com>
2023-01-07 18:59:13 -0300

    Calculator without input in Python

---
+++ calculadora.py
@@ -0,0 +1,25 @@
+class Calculadora:
+    def __init__(self):
+        pass
+       
+    def soma(self,valor_a,valor_b):
+        return valor_a + valor_b
+
+    def subtracao(self,valor_a,valor_b):
+        return valor_a - valor_b
+
+    def multiplicacao(self,valor_a,valor_b):
+        return valor_a * valor_b
+
+    def divisao(self,valor_a,valor_b):
+        return valor_a / valor_b
+
+"""  print (soma(1,2))
+    print (soma(3,4))
+    print(subtracao(10,2)) """
+
+calculadora = Calculadora ()
+print (calculadora.soma(10,2))
+print (calculadora.subtracao(10,2))
+print (calculadora.multiplicacao(10,2))
+print (calculadora.divisao(10,2))
